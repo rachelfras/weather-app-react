5dd25720ebf6ee9431c1a7d533e32fac41a83a06
rachelfras <82391537+rachelfras@users.noreply.github.com>
2021-10-17 23:35:45 +0100

    Moved form to App component
    Needed to be able to send response from form call to other components on App.js
    
    - not sure if able to send response back with return HTML

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+import React, { useState } from "react";
+import axios from "axios";
 import './App.css';
-import Form from "./Form.js";
 import Temperature from "./Temperature";
 import IconDisplay from "./IconDisplay";
 import PlaceTime from "./PlaceTime";
@@ -8,11 +9,72 @@ import Forecast from "./Forecast";
 import Links from "./Links";
 
 function App() {
+  const [city, setCity] = useState(null);
+  const [data, setData] = useState(null);
+
+  function handleResponse(response){
+    setData(response.data);
+  }
+
+  function handleSubmit(event){
+    event.preventDefault();
+    let apiKey = `fa1047ba99073894a88e54f4a5673a70`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    
+    axios.get(url).then(handleResponse);
+  }
+
+  function handleValue(event){
+    setCity(event.target.value);
+  }
+
+  function displayCity(response){
+    let current = `${response.data.name}, ${response.data.sys.country}`;
+    alert(current);
+  }
+
+  function giveCurrent(position) {
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
+    let units = "metric";
+    let apiKey = `fa1047ba99073894a88e54f4a5673a70`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}`;
+    
+    axios.get(`${apiUrl}&units=${units}&appid=${apiKey}`).then(displayCity);
+}
+
+  function searchGeoLocation(event){
+    event.preventDefault();
+    navigator.geolocation.getCurrentPosition(giveCurrent);
+  }
+
   return (
     <div className="App">
       <div className="widget">
       <header>
-        <Form />
+        <form onSubmit={handleSubmit}>
+        <div className="input-group">
+          <input type="text"
+              className="form-control form-bar enter"
+              placeholder="Enter City"
+              onChange={handleValue}
+          />
+          <div className="input-group-append" id="button-addon4">
+            <input
+              type="submit"
+              className="btn btn-outline-secondary search form-bar"
+              value="Search"
+            />
+            <button
+              className="btn btn-outline-secondary current form-bar"
+              type="button"
+              onClick={searchGeoLocation}
+            >
+              Current
+            </button>
+          </div>
+        </div>
+      </form>
       </header>
       <section className="current-weather">
           <div className="row">
@@ -20,7 +82,7 @@ function App() {
               <div className="temp-box">
                 <div className="row">
                   <div className="col-6">
-                    <Temperature />
+                    <Temperature data={data}/>
                   </div>
                   <div className="col-6">
                     <IconDisplay />
