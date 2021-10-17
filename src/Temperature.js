96c599117662fb489d97bd99a28f437a9203fd37
rachelfras <82391537+rachelfras@users.noreply.github.com>
2021-10-17 23:36:15 +0100

    Linked Temperature to response call

--- src/Temperature.js
+++ src/Temperature.js
@@ -1,14 +1,17 @@
 import React from "react";
 
-export default function Temperature(){
+export default function Temperature(props){
+  console.log(props.data);
+
     return (
     <div className="Temperature">
       <h1>
-        <span id="big-temp">12</span>°C
+        <span id="big-temp">{Math.round(props.data.main.temp)}</span>°C
       </h1>
       <hr />
       <p>
-        <span id="max">14</span>°/<span id="min">11</span>°
+        <span id="max">{Math.round(props.data.main.temp_max)}
+        </span>°/<span id="min">{Math.round(props.data.main.temp_min)}</span>°
       </p>
     </div>
   );
